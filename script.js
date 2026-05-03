0fa6cd92dcaac646e13b05665a19f365f8a1e23a
debarpanpal9-hue <debarpanpal9@gmail.com>
2026-05-03 17:25:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function addLoadout() {
+  const gun = document.getElementById("gunName").value;
+  const attachments = document.getElementById("attachments").value;
+  const file = document.getElementById("imageInput").files[0];
+
+  if (!file) return alert("Upload an image!");
+
+  const reader = new FileReader();
+  reader.onload = function () {
+    const data = {
+      gun,
+      attachments,
+      image: reader.result
+    };
+
+    let loadouts = JSON.parse(localStorage.getItem("loadouts")) || [];
+    loadouts.push(data);
+    localStorage.setItem("loadouts", JSON.stringify(loadouts));
+
+    displayLoadouts();
+  };
+
+  reader.readAsDataURL(file);
+}
+
+function displayLoadouts() {
+  const container = document.getElementById("loadoutContainer");
+  container.innerHTML = "";
+
+  let loadouts = JSON.parse(localStorage.getItem("loadouts")) || [];
+
+  loadouts.forEach(l => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <img src="${l.image}">
+      <h3>${l.gun}</h3>
+      <p>${l.attachments}</p>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+displayLoadouts();
